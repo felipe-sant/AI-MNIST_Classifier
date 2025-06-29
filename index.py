24d6fdafc719badaa23eb48ec4a6ec4becfdde4c
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-06-29 16:26:04 -0300

    fix: ajustando rota de teste

--- index.py
+++ index.py
@@ -8,16 +8,17 @@ absl.logging.set_verbosity(absl.logging.ERROR)
 
 from fastapi import FastAPI, UploadFile, File
 from tensorflow import keras
-from src.functions.indetifyImage import indenfityImage
+
+from src.functions.identifyImage import identifyImage  # Certifique-se do nome correto
 
 app = FastAPI()
 model = keras.models.load_model("mnist_model.h5")
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    predict = await indenfityImage(model, file)
-    return { "predict": predict }
+    prediction = await identifyImage(model, file)
+    return { "predict": prediction }
 
 @app.get("/")
 async def test():
-    return "IAService is working!"  
+    return {"message": "IAService is working!"}
