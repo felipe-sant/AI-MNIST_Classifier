1d0ba9ea3ffa908fa68e55463d6c40182411e19c
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-06-29 16:46:15 -0300

    fix: arrumando rota predict

--- index.py
+++ index.py
@@ -1,23 +1,23 @@
-import os
-import tensorflow as tf
-import warnings
-import absl.logging
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-warnings.filterwarnings("ignore")
-absl.logging.set_verbosity(absl.logging.ERROR)
-
 from fastapi import FastAPI, UploadFile, File
 from tensorflow import keras
-
-from src.functions.identifyImage import identifyImage
+from fastapi import File, UploadFile
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import io
 
 app = FastAPI()
 model = keras.models.load_model("mnist_model.h5")
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    prediction = await identifyImage(model, file)
-    return { "predict": prediction }
+    img = Image.open(io.BytesIO(await file.read())).convert('L').resize((28, 28))
+    img_array = np.array(img).astype('float32') / 255.0
+    img_array = 1.0 - img_array
+    img_array = np.expand_dims(img_array, axis=(0, -1))
+    prediction = model.predict(img_array)
+    predicted_digit = int(np.argmax(prediction[0]))
+    return { "predict": predicted_digit }
 
 @app.get("/")
 async def test():
